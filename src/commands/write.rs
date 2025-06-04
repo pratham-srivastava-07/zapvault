3b50130b323397c1801c10ebb02187e5ecbe21e0
Krish <prathamrajsri2003@gmail.com>
2025-06-05 03:22:19 +0530

    chore: write command

--- src/commands/write.rs
+++ src/commands/write.rs
@@ -1,7 +1,38 @@
-use std::io::{self, Write};
-
+use std::{fs, io::{self, Write}};
+use crate::meta::{Vault, VaultWrite};
+// use crate::meta::Vault;
+// use std::{fs};
 
 pub fn handle_write(tag: Option<String>) {
+
+    // ask for vault path 
+    print!("Enter vault path: ");
+    io::stdout().flush().unwrap();
+    let mut path = String::new();
+    io::stdin().read_line(&mut path).unwrap();
+    let path = path.trim().trim_matches('"');
+    let path_input = std::path::PathBuf::from(path);
+    println!("{}", path);
+    print!("{:?}", path_input);
+
+    // search if the given folder path consists of vault.meta file
+    let meta_data = path_input.join("vault.meta");
+    if !meta_data.exists() {
+        eprintln!("vault.meta not found at {:?}", meta_data);
+        return;
+    } else {
+        println!("Vault found");
+    }
+    // getting file contents
+    let file = fs::File::open(&meta_data).expect("Failed to open vault.meta file at location at given location");
+    let vault: Vault = serde_json::from_reader(file).expect("Failed to parse vault meta data");
+    
+    let vault_entries = VaultWrite {
+        password_hash: vault.password_hash.clone(),
+        entries: vec![vault]
+    };
+
+    println!("{:?}",vault_entries);
 
     let tag = match tag {
         Some(t) => t,
--- src/meta.rs
+++ src/meta.rs
@@ -1,8 +1,13 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Vault {
     pub password_hash: String,
     pub ciphertext: Vec<u8>,
     pub nonce: [u8; 12],
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct VaultWrite {
+    pub password_hash: String,
+    pub entries: Vec<Vault>,
 }
